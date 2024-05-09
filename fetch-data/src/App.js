8ce0faf4d53ac418719b3726a441b1f05bf52614
azman08 <114061152+azman08@users.noreply.github.com>
2024-05-09 14:19:05 +0530

    Update App.js

--- fetch-data/src/App.js
+++ fetch-data/src/App.js
@@ -6,15 +6,12 @@ import React, { Component } from "react";
 class App extends Component {
   constructor(props) {
     super(props);
-
-    // Set initial state
     this.state = { users_data: [], loading: true };
 
     this.updateState = this.updateState.bind(this);
   }
 
   updateState() {
-    // document.getElementById("main").style.display='flex';
     const link = "https://reqres.in/api/users?page=1";
     fetch(link)
       .then((response) => response.json())
